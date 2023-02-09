aa912a051fb15db58993bf5a609ae3cf88351da1
villalobos1727 <112015959+villalobos1727@users.noreply.github.com>
2023-02-09 14:48:01 -0300

    Add files via upload

---
+++ PROYECTO-UNO-JOIAS-VILLA-main/footer.php
@@ -0,0 +1,54 @@
+<footer class="">
+<div class="container-fluid">
+      <div class="row text-center bg-light">
+        <div class="col-md-3">
+          <h4>Institucional</h4>
+          <p><a href="#">Home</a></p>
+          <p><a href="#">Nossa Loja</a></p>
+          <p><a href="#">Conheça nossas coleçoes</a></p>
+          <p><a href="#">Área do Cliente</a></p>
+        </div>
+
+        <div class="col-md-3">
+          <h4>Redes Sociales</h4>
+          <ul class="navbar-nav mr-auto">
+            <li class="navbar-item"><a class="nav-link" href="#">Facebook</a></li>
+            <li class="navbar-item"><a class="nav-link" href="#">Instagram</a></li>
+            <li class="navbar-item"><a class="nav-link" href="#">tick-tock</a></li>
+            <li class="navbar-item"><a class="nav-link" href="#">whatsapp</a></li>
+            <li class="navbar-item"><a class="nav-link" href="#">Local</a></li>
+          </ul>
+        </div>
+        
+        <div class="col-md-3">
+          <h4>Funcionamento</h4>
+          <p>Segunda à Sábado - 06:00 às 23:00</p>
+          <p>Domingo - 07:00 às 13:00</p>
+        </div>  
+        
+        <div class="col-md-3 mapa">
+          <h4>Localização</h4>
+          <iframe src=""></iframe>
+        </div>
+      </div>
+
+      <div class="row text-center">
+        <div class="col-md-12 sociais">
+          <a href="#"><img src="imagens/whatsapp.png"></a>
+          <a href="#"><img src="imagens/face.png"></a>
+          <a href="#"><img src="imagens/insta.png"></a>
+         
+        </div>
+      </div>
+    </div>
+  </footer>
+
+     <!-- JavaScript (Opcional) -->
+    <!-- jQuery primeiro, depois Popper.js, depois Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  </body>
+</html>
+        
+        
