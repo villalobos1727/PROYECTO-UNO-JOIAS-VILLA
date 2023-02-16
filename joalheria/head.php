8cea9a68cbfb43cc5b436dd44d96345b77ac60f6
villalobos1727 <112015959+villalobos1727@users.noreply.github.com>
2023-02-16 13:19:14 -0300

    Add files via upload

---
+++ joalheria/head.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <script src="https://kit.fontawesome.com/3df637a2f2.js" crossorigin="anonymous"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
+  <!-- Meta tags Obrigatórias -->
+  <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+    <title>VILLA JOALHERIA</title>
+</head>
+<body>
---
+++ joalheria/index.php
@@ -0,0 +1,91 @@
+<?php
+    require_once 'head.php';
+    require_once 'menu.php';
+
+
+?>
+
+    <section class="first-section" id="home">
+        
+        <div class="conteudo-principal">
+            <h1></h1>
+            <h2>Deixe-nos realizar Seu Sonho,Temos As Melhores Joias</h2>
+            <div class="btn">
+                <button class="reservar">Reserve a sua!</button>
+                <button class="montar">Monte seu pedido!</button>
+            </div>
+        </div>    
+    </section>    
+
+    <section class="sobre-nos" id="sobrenos"> 
+        <div class="main">
+            <div class="contentsobre">
+                <h2>Sobre nós</h2>
+                <p>Nossa joalheria é unica somos epecialistas em realizar sonhos,nossa empresa nasceu do sonho de 3 amigas que são apaixonada por joias e beleza,nossos produtos são confeccionados com tecnologia e obra prima,tudo com um toque único nossas peças são feitas sob encomenda,deixe-nos realizar seus sonhos. 
+                </p>
+            </div>     
+            <div class="img-joias">
+                <img src="./img/VJ.png" alt="PÉROLA">
+            </div>
+        </div>    
+    </section>
+
+    <section class="joias" id="joias">
+        <h2>Nossas Joias</h2>
+
+            <div class="container-fluid itens-">
+                <div class="row">
+
+                    <div class="col-md-3">
+                        <div class="pulseira bebê">
+                             <img src="./img/feminina.webp" alt="pulseira" style=width:300px;height:300px;>
+                            <div class="info">
+                                <h3>Coleção feminina</h3>                                
+                               
+                            </div>   
+                        </div>
+                    </div>
+
+
+                    <div class="col-md-3">
+                        <div class="pulseira bebê">
+                            <img src="./img/masculino.jpg" alt="pulseira" style=width:300px;height:300px;>
+                            <div class="info">
+                                <h3>coleção masculina</h3>
+                                
+                            </div>                         
+                        </div>
+                    </div>
+
+                <div class="col-md-3">
+                    <div class="pulseira">
+                        <img src="./img/infantil.jpeg" alt="colar" style=width:300px;height:300px;>
+                        <div class="info">
+                            <h3>Coleção infantil</h3>
+                            
+                        </div>                 
+                    </div>
+                </div>
+
+                <div class="col-md-3">
+                    <div class="anéis">
+                        <img src="./img/acessorio.webp" alt="anél" style=width:300px;height:300px;>
+                        <div class="info">
+                            <h3>Acessorios</h3>
+                            ,
+                        </div>                 
+                    </div>
+                </div>
+
+            </div>
+
+
+                       </div>
+
+    </section>
+
+   <?php
+
+   require_once 'footer.php';
+
+   ?>
